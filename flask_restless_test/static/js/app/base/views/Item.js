f0c99fe35ba031460e13d11645cd42456547dd29
Dima Bildin <bildja2@gmail.com>
2014-02-05 21:19:14 +0200

    A little refactor of the item views

--- flask_restless_test/static/js/app/base/views/Item.js
+++ flask_restless_test/static/js/app/base/views/Item.js
@@ -15,47 +15,6 @@ define(function (require) {
             'click .delete-item': 'deleteItem'
         },
 
-        initialize: function () {
-            this.listenTo(this.model, 'change:owner.name', this.renderOwner);
-            this.listenTo(this.model, 'change:name', this.renderName);
-            this.listenTo(this.model, 'change:vendor', this.renderVendor);
-            this.listenTo(this.model, 'change:purchase_time', this.renderPurchaseTime);
-        },
-
-        renderOwner: function () {
-            var $computerOwner = this.$('.computer-owner'),
-                $ownerLink = $computerOwner.find('a');
-            if (!$ownerLink.length) {
-                $ownerLink = $('<a></a>').appendTo($computerOwner);
-            }
-            $ownerLink
-                    .attr('href', '/person/edit/' + this.model.get('owner').id)
-                    .text(this.model.getOwnerName());
-            $computerOwner.tomato();
-            return this;
-        },
-
-        renderName: function () {
-            this.$('.computer-name')
-                .text(this.model.get('name'))
-                .tomato();
-            return this;
-        },
-
-        renderVendor: function () {
-            this.$('.computer-vendor')
-                .text(this.model.get('vendor'))
-                .tomato();
-            return this;
-        },
-
-        renderPurchaseTime: function () {
-            this.$('.computer-purchase-time')
-                .text(this.model.getPurchaseTimeFormatted())
-                .tomato();
-            return this;
-        },
-
         render: function () {
             this.$el.html(this.template({
                 model: this.model
--- flask_restless_test/static/js/app/computers/views/ComputerItem.js
+++ flask_restless_test/static/js/app/computers/views/ComputerItem.js
@@ -5,6 +5,47 @@ define(function (require) {
         computerItemTemplate = require('text!templates/computers/computer-item.html');
     return BaseItemView.extend({
         template: _.template(computerItemTemplate),
-        className: 'computer-item'
+        className: 'computer-item',
+
+        initialize: function () {
+            this.listenTo(this.model, 'change:owner.name', this.renderOwner);
+            this.listenTo(this.model, 'change:name', this.renderName);
+            this.listenTo(this.model, 'change:vendor', this.renderVendor);
+            this.listenTo(this.model, 'change:purchase_time', this.renderPurchaseTime);
+        },
+
+        renderOwner: function () {
+            var $computerOwner = this.$('.computer-owner'),
+                $ownerLink = $computerOwner.find('a');
+            if (!$ownerLink.length) {
+                $ownerLink = $('<a></a>').appendTo($computerOwner);
+            }
+            $ownerLink
+                    .attr('href', '/person/edit/' + this.model.get('owner').id)
+                    .text(this.model.getOwnerName());
+            $computerOwner.tomato();
+            return this;
+        },
+
+        renderName: function () {
+            this.$('.computer-name')
+                .text(this.model.get('name'))
+                .tomato();
+            return this;
+        },
+
+        renderVendor: function () {
+            this.$('.computer-vendor')
+                .text(this.model.get('vendor'))
+                .tomato();
+            return this;
+        },
+
+        renderPurchaseTime: function () {
+            this.$('.computer-purchase-time')
+                .text(this.model.getPurchaseTimeFormatted())
+                .tomato();
+            return this;
+        }
     });
 });
